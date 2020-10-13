b1be34c223cc539618b3fa93684647268c97cfb0
Sanjana Ramesh <sanjana8838@gmail.com>
2020-10-13 13:48:55 +0530

    new code but increment not working

--- top.v
+++ top.v
@@ -4,47 +4,17 @@ module top (
     output PIN_13,   // User/boot LED next to power LED
     input PIN_12  // USB pull-up resistor
 );
-    wire clk_enable;
-    reg[27:0] counter=0;
-    wire tmp1, tmp2, inc;
-    reg[3:0] counterPWM = 0;
-    reg[3:0] dutycycle = 5; //50% duty cycle
-    always @(posedge CLK)
-    begin
-      counter <= counter+1;
-      if(counter>=1)
-        counter <=0;
-      end
-      assign clk_enable = counter == 1 ?1:0;
-      dffpwm dff1(CLK, clk_enable, PIN_12, tmp1);
-      dffpwm dff2(CLK, clk_enable, tmp1, tmp2);
-      assign inc = tmp1 & (~tmp2) & clk_enable;
-      always @(posedge CLK)
-      begin
-        if (inc ==1 && dutycycle <=9)
-          dutycycle <= dutycycle + 1;
-        else
-          dutycycle = 5;
-      end
-      always @(posedge CLK)
-      begin
-        counterPWM <= counterPWM + 1;
-        if (counterPWM>=9)
-          counterPWM <= 0;
-      end
-      assign PIN_13 = counterPWM < dutycycle ? 1:0;
-
-endmodule
-
-module dffpwm (
-  input clk,
-  input en,
-  input D,
-  output reg Q
-  );
-  always @(posedge clk)
+  reg [7:0] counter = 0;
+  reg [7:0] dutycyle=20;
+  always @(posedge CLK)
   begin
-    if (en==1)
-      Q <= D;
+    if (counter <100) counter <= counter + 1;
+    else counter <= 0;
   end
+  always @(posedge CLK)
+  begin
+    if (PIN_12 == 1 && dutycycle<=90) dutycycle = dutycycle + 10;
+    else dutycycle = 20;
+  end
+  assign PIN_13 = (counter<50) ? 1:0;
 endmodule
