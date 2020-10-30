6c72d962d4a395d7b953e30bcd2d91ecc40128db
Sanjana Ramesh <65151957+sanjana8838@users.noreply.github.com>
2020-10-30 17:56:08 +0530

    Working code final

--- top.v
+++ top.v
@@ -1,20 +1,18 @@
-// look in pins.pcf for all the pin names on the TinyFPGA BX board
 module top (
-    input CLK,    // 16MHz clock
-    output PIN_13,   // User/boot LED next to power LED
-    input PIN_12  // USB pull-up resistor
+    input CLK,
+    output PIN_13,
+    input PIN_12
 );
   reg [7:0] counter = 0;
-  reg [7:0] dutycyle=20;
+  reg [7:0] dutycycle = 50;
+
+
   always @(posedge CLK)
   begin
     if (counter <100) counter <= counter + 1;
     else counter <= 0;
+
   end
-  always @(posedge CLK)
-  begin
-    if (PIN_12 == 1 && dutycycle<=90) dutycycle = dutycycle + 10;
-    else dutycycle = 20;
-  end
-  assign PIN_13 = (counter<50) ? 1:0;
+
+  assign PIN_13 = (counter<dutycycle) ? 1:0;
 endmodule
